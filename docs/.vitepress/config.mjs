3fa96ed43cd8ef3d1ccdc1b190942eaa055928df
cjy <1404340013@qq.com>
2023-12-21 16:52:31 +0800

    feat:修改配置文件

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vitepress/',
+  lang: 'zh-CN',
   title: "前端小站",
   description: "日常知识积累",
   themeConfig: {
