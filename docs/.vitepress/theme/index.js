45a41e6862590838671f373fca7cdf7d78c2df87
cjy <1404340013@qq.com>
2025-10-14 15:14:54 +0800

    ✨ feat(图片): 添加查看图片插件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,7 +3,10 @@ import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
 import Card from "./components/Card.vue";
-
+import "viewerjs/dist/viewer.min.css";
+import imageViewer from "vitepress-plugin-image-viewer";
+import vImageViewer from "vitepress-plugin-image-viewer/lib/vImageViewer.vue";
+import { useRoute } from "vitepress";
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
@@ -14,5 +17,11 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     app.component("Card", Card);
+    app.component("vImageViewer", vImageViewer);
+  },
+  setup() {
+    const route = useRoute();
+    // 启用插件
+    imageViewer(route);
   },
 };
